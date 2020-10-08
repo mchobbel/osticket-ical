dddff36312f579349ad1a4be2e16ddb019cd62d6
Michiel Hobbelman <mch@mc-h.nl>
2020-10-08 18:34:39 +0200

    improve documentation

--- qware_ical/plugin.php
+++ qware_ical/plugin.php
@@ -5,7 +5,7 @@ return array(
     'version' =>        '0.1.1',
     'name' =>            'Qware Ical handler',
     'author' =>         'Michiel Hobbelman',
-    'description' =>    'Deal with incoming ical event email',
+    'description' =>    'Deal with incoming ical/ics event email',
     'url' =>            'https://github.com/mchobbel/osticket-ical',
     'plugin' =>         'bootstrap.php:QwareIcalPlugin'
 );
